c7407e374feeeaa1ae4f23a3894f4d999f65bf62
Umi-Neko8 <165974321+Umi-Neko8@users.noreply.github.com>
2024-06-01 16:32:39 +0900

    修正分

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,7 @@
 let userHeight = 1.7;
 let userWeight = 68;
-let userBmi = 68 / (1.7 * 1.7);
+
+userBmi = userWeight / (userHeight * userHeight)
 
 console.log(userHeight);
 console.log(userWeight);
